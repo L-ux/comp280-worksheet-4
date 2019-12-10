0286df829e680d7995d363062d958816e3f323a5
JOSHUA GRILLO <JG215923@falmouth.ac.uk>
2019-12-10 11:03:32 +0000

    Started work on changes after some heuristic analysis

--- UI stuff/Assets/Scripts/PauseButton.cs
+++ UI stuff/Assets/Scripts/PauseButton.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class PauseButton : MonoBehaviour, IPointerDownHandler
+public class PauseButton : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public MenuController.Menus targetMenu;
+
+    public Sprite normalImage;
+    public Sprite hoverImage;
+
 
     public void OnPointerDown(PointerEventData eventData)
     {
         MenuController.changeMenu(targetMenu);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Image>().sprite = hoverImage;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Image>().sprite = normalImage;
     }
 }
