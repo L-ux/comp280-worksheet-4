bf239ea1a3489b2f67b39279f92234623e11a1dd
L-ux <Josh@Grillo.co.uk>
2019-12-12 18:54:07 +0000

    Final product after heuristic analysis'

---
+++ UI stuff/Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    public float distance;
+    public float speed;
+    float dTime = 0.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        dTime += Time.deltaTime;
+
+        transform.position = new Vector3(distance * Mathf.Sin(dTime * speed),1.103f, distance * Mathf.Cos(dTime * speed));
+    }
+}
